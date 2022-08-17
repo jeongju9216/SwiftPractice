c611faaa57ea7f342d84eaec980ec39656de12e2
유정주 <dbwjdwn9216@gmail.com>
2022-08-17 21:52:27 +0900

    Add: DispatchTime과 DispatchWallTime

---
+++ SwiftPractice/SwiftPractice/GCD/DispatchWallTime.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  SwiftPractice
+//
+//  Created by 유정주 on 2022/08/17.
+//
+
+import Foundation
+
+DispatchQueue.global().asyncAfter(wallDeadline: .now() + 10) {
+    // 코드
+    print("HERE")
+}
+
+while true { }
