ac62898112f7553dfba22bdc8825f55bf1d0bf25
유정주 <dbwjdwn9216@gmail.com>
2023-01-24 21:19:10 +0900

    Add: 코어데이터 CRUD 추가

--- CoreDataExample/CoreDataExample/ViewController.swift
+++ CoreDataExample/CoreDataExample/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import CoreData
+import Combine
 
 final class ViewController: UIViewController {
 
@@ -14,7 +15,9 @@ final class ViewController: UIViewController {
     var viewContext: NSManagedObjectContext!
     
     private let searchController: UISearchController = UISearchController()
-        
+    
+    private var cancellables: Set<AnyCancellable> = []
+            
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -22,10 +25,10 @@ final class ViewController: UIViewController {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         self.container = appDelegate.persistentContainer
         self.viewContext = container.viewContext
-        
-        navigationItem.searchController = self.searchController
-        
+                
         setupSearchBar()
+        
+        bind()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -38,21 +41,41 @@ final class ViewController: UIViewController {
     }
     
     //MARK: - Methods
+    func bind() {
+        searchController.searchBar.searchTextField
+            .debounceSearchPublisher
+            .sink { [weak self] receivedValue in
+                guard let self = self else {
+                    return
+                }
+                
+                print("receivedValue: \(receivedValue)")
+                if !receivedValue.isEmpty {
+                    self.update(keyword: receivedValue)
+                }
+            }
+            .store(in: &cancellables)
+    }
+    
     func setupSearchBar() {
+        navigationItem.searchController = self.searchController
+
         searchController.searchBar.placeholder = "검색하세요."
         searchController.searchBar.delegate = self
     }
     
     func saveKeyword(keyword: String) {
+        print("[Save] \(keyword)")
+        
         let entity = NSEntityDescription.entity(forEntityName: "Keyword", in: self.viewContext)
         
         guard let entity = entity else {
             return
         }
         
-        let object = NSManagedObject(entity: entity, insertInto: self.viewContext)
-        object.setValue(keyword, forKey: "keyword")
-        object.setValue(Date(), forKey: "createdAt")
+        let object = Keyword(entity: entity, insertInto: self.viewContext)
+        object.keyword = keyword
+        object.createdAt = Date()
         
         do {
             try self.viewContext.save()
@@ -60,26 +83,39 @@ final class ViewController: UIViewController {
             print(error.localizedDescription)
         }
     }
-
-    func fetchContact() {
-        let date = Date()
-        print("current Date: \(date)")
+    
+    func update(keyword: String) {
+        let fetchRequest = Keyword.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "keyword == %@", keyword)
         
         do {
-            let contact = try self.container.viewContext.fetch(Keyword.fetchRequest()) as! [Keyword]
+            let contact = try self.viewContext.fetch(fetchRequest)
+            
+            if contact.isEmpty {
+                saveKeyword(keyword: keyword)
+            } else {
+                print("[Update] \(keyword)")
+                
+                let updatedObject: Keyword = contact[0]
+                
+                updatedObject.setValue(Date(), forKey: "createdAt")
+                
+                try self.viewContext.save()
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+
+    func fetchContact() {
+        do {
+            let fetchRequest = Keyword.fetchRequest()
+            
+            let contact = try self.viewContext.fetch(fetchRequest)
             
             for data in contact {
-                let distanceSecond: DateComponents = Calendar.current.dateComponents([.minute, .second], from: data.createdAt!, to: date)
-                if let diffMin = distanceSecond.minute,
-                        diffMin >= 5 {
-                    print("Delete!!")
-                    self.container.viewContext.delete(data)
-                } else {
-                    print("keyword: \(data.keyword) / createdAt: \(data.createdAt)")
-                }
+                print("[Fetch] keyword: \(data.keyword) / createdAt: \(data.createdAt)")
             }
-            
-            try self.container.viewContext.save()
         } catch {
             print(error.localizedDescription)
         }
@@ -90,15 +126,6 @@ extension ViewController: UISearchBarDelegate {
     //텍스트 입력을 시작할 때
     func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
         self.searchController.searchBar.setShowsCancelButton(true, animated: true)
-    }
-    
-    //텍스트가 변경될 때
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        guard let text = searchBar.text else {
-            return
-        }
-        
-        print("Input: \(text)")
     }
     
     //검색 버튼을 눌렀을 때
@@ -125,5 +152,17 @@ extension ViewController: UISearchBarDelegate {
     private func dismissKeyboard() {
         self.searchController.searchBar.resignFirstResponder()
         self.searchController.searchBar.setShowsCancelButton(false, animated: true)
+    }
+}
+
+extension UISearchTextField {
+    var debounceSearchPublisher: AnyPublisher<String, Never> {
+        NotificationCenter.default.publisher(
+            for: UISearchTextField.textDidChangeNotification,
+            object: self)
+            .compactMap { $0.object as? UISearchTextField }
+            .map { $0.text ?? "" }
+            .debounce(for: .milliseconds(1000), scheduler: RunLoop.main)
+            .eraseToAnyPublisher()
     }
 }
