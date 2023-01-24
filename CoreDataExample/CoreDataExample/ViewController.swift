10b8dbec8822f341e13abd277c260f6ac5678022
유정주 <dbwjdwn9216@gmail.com>
2023-01-24 22:47:26 +0900

    Add: CollectionView 추가

--- CoreDataExample/CoreDataExample/ViewController.swift
+++ CoreDataExample/CoreDataExample/ViewController.swift
@@ -18,6 +18,15 @@ final class ViewController: UIViewController {
     
     private var cancellables: Set<AnyCancellable> = []
             
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    enum Section: CaseIterable {
+        case main
+    }
+    
+    var keywords: [String] = []
+    var dataSource: UICollectionViewDiffableDataSource<Section, String>!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -28,16 +37,19 @@ final class ViewController: UIViewController {
                 
         setupSearchBar()
         
+        removeAllCoreData()
+        
+        self.keywords = fetchContact()
+        
+        self.collectionView.collectionViewLayout = createLayout()
+        setupDataSource()
+        performQuery()
+        
         bind()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         dismissKeyboard()
-    }
-    
-    //MARK: - Actions
-    @IBAction func clickedButton(_ sender: UIButton) {
-        fetchContact()
     }
     
     //MARK: - Methods
@@ -52,11 +64,62 @@ final class ViewController: UIViewController {
                 print("receivedValue: \(receivedValue)")
                 if !receivedValue.isEmpty {
                     self.update(keyword: receivedValue)
+                    
+                    self.keywords = self.fetchContact()
+                    
+                    self.performQuery()
                 }
             }
             .store(in: &cancellables)
     }
     
+    func setupDataSource() {
+        self.collectionView.register(KeywordCollectionViewCell.self, forCellWithReuseIdentifier: KeywordCollectionViewCell.identifier)
+        self.dataSource = UICollectionViewDiffableDataSource<Section, String>(collectionView: self.collectionView) { [weak self] (collectionView, indexPath, dj) -> UICollectionViewCell? in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: KeywordCollectionViewCell.identifier,
+                                                                for: indexPath) as? KeywordCollectionViewCell,
+                let self = self else {
+                preconditionFailure()
+            }
+            
+            cell.configure(systemName: "iphone", keyword: self.keywords[indexPath.item])
+            
+            return cell
+        }
+    }
+    
+    func createLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection in
+            let columns = 1
+            let spacing = CGFloat(10)
+            
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                  heightDimension: .fractionalHeight(1.0))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                   heightDimension: .absolute(50))
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, repeatingSubitem: item, count: columns)
+            group.interItemSpacing = .fixed(spacing)
+            
+            let section = NSCollectionLayoutSection(group: group)
+            section.interGroupSpacing = spacing
+            section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+            
+            return section
+        }
+        
+        return layout
+    }
+    
+    func performQuery() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, String>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(keywords)
+        
+        self.dataSource.apply(snapshot, animatingDifferences: true)
+    }
+
     func setupSearchBar() {
         navigationItem.searchController = self.searchController
 
@@ -107,7 +170,8 @@ final class ViewController: UIViewController {
         }
     }
 
-    func fetchContact() {
+    func fetchContact() -> [String] {
+        var keywords: [String] = []
         do {
             let fetchRequest = Keyword.fetchRequest()
             
@@ -115,7 +179,27 @@ final class ViewController: UIViewController {
             
             for data in contact {
                 print("[Fetch] keyword: \(data.keyword) / createdAt: \(data.createdAt)")
+                keywords.append(data.keyword ?? "")
             }
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        return keywords
+    }
+    
+    func removeAllCoreData() {
+        do {
+            let fetchRequest = Keyword.fetchRequest()
+            
+            let contact = try self.viewContext.fetch(fetchRequest)
+            
+            for data in contact {
+                print("[Fetch] keyword: \(data.keyword) / createdAt: \(data.createdAt)")
+                self.viewContext.delete(data)
+            }
+            
+            try self.viewContext.save()
         } catch {
             print(error.localizedDescription)
         }
@@ -126,6 +210,10 @@ extension ViewController: UISearchBarDelegate {
     //텍스트 입력을 시작할 때
     func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
         self.searchController.searchBar.setShowsCancelButton(true, animated: true)
+        
+        UIView.animate(withDuration: 0.05, delay: 0.3) {
+            self.collectionView.alpha = 1
+        }
     }
     
     //검색 버튼을 눌렀을 때
@@ -138,7 +226,7 @@ extension ViewController: UISearchBarDelegate {
         
         print("Search: \(text)")
         
-        saveKeyword(keyword: text)
+        update(keyword: text)
     }
     
     //취소 버튼을 눌렀을 때
@@ -146,6 +234,8 @@ extension ViewController: UISearchBarDelegate {
         dismissKeyboard()
         
         self.searchController.searchBar.text = ""
+        
+        self.collectionView.alpha = 0
     }
     
     //키보드 숨기기
@@ -164,5 +254,52 @@ extension UISearchTextField {
             .map { $0.text ?? "" }
             .debounce(for: .milliseconds(1000), scheduler: RunLoop.main)
             .eraseToAnyPublisher()
+    }
+}
+
+class KeywordCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "KeywordCollectionViewCell"
+    
+    var iconImageView: UIImageView!
+    var keywordLabel: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    func setup() {
+        self.contentView.layer.cornerRadius = 10
+        self.contentView.layer.borderWidth = 1
+        self.contentView.layer.borderColor = UIColor.black.cgColor
+        
+        iconImageView = UIImageView()
+        iconImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        keywordLabel = UILabel()
+        keywordLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        keywordLabel.textAlignment = .center
+        
+        self.contentView.addSubview(iconImageView)
+        self.contentView.addSubview(keywordLabel)
+        
+        NSLayoutConstraint.activate([
+            iconImageView.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor),
+            iconImageView.leftAnchor.constraint(equalTo: self.contentView.leftAnchor, constant: 10),
+            
+            keywordLabel.leftAnchor.constraint(equalTo: iconImageView.rightAnchor, constant: 10),
+            keywordLabel.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor),
+        ])
+    }
+    
+    func configure(systemName: String, keyword: String) {
+        self.iconImageView.image = UIImage(systemName: systemName)
+        self.keywordLabel.text = keyword
     }
 }
