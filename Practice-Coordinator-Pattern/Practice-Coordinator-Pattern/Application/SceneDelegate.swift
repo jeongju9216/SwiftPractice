0c8821b56e148c1522f748d3b971d74ff6dc8c25
유정주 <dbwjdwn9216@gmail.com>
2023-03-24 21:37:27 +0900

    AppCoordinator 참조 수정

--- Practice-Coordinator-Pattern/Practice-Coordinator-Pattern/Application/SceneDelegate.swift
+++ Practice-Coordinator-Pattern/Practice-Coordinator-Pattern/Application/SceneDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    var appCoordinator: AppCoordinator?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -21,8 +21,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let window = UIWindow(windowScene: windowScene)
         self.window = window
         
-        let appCoordinator = AppCoordinator(window: window)
-        appCoordinator.start()
+        appCoordinator = AppCoordinator(window: window)
+        appCoordinator?.start()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
