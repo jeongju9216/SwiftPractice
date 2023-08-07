c64aa2651e55ae5615fca8b94d7707f6cad643d5
유정주 <dbwjdwn9216@gmail.com>
2023-08-07 10:08:07 +0900

    convenience init

--- InitializerExample.playground/Contents.swift
+++ InitializerExample.playground/Contents.swift
@@ -2,12 +2,35 @@ import UIKit
 
 class ClassA {
     var num: Int
+    var name: String
     
-    init(num: Int) {
+    init(num: Int, name: String) {
         self.num = num
+        self.name = name
+    }
+    
+    convenience init(num: Int) {
+        self.init(num: num, name: "jeong")
     }
 }
 
+
 struct StructA {
     var num: Int
+    var name: String
+    
+    init(num: Int, name: String) {
+        self.num = num
+        self.name = name
+    }
+    
+    init(num: Int) {
+        self.init(num: num, name: "jeong")
+    }
 }
+
+let classA = ClassA(num: 10)
+let structA = StructA(num: 10)
+
+print(classA.num)
+print(structA)
