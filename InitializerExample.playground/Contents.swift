c95b42cbf1f0392faa1b0c28d92b6c2f1ad061c9
유정주 <dbwjdwn9216@gmail.com>
2023-08-07 14:40:13 +0900

    멤버와이즈 초기화

--- InitializerExample.playground/Contents.swift
+++ InitializerExample.playground/Contents.swift
@@ -14,23 +14,12 @@ class ClassA {
     }
 }
 
-
 struct StructA {
     var num: Int
     var name: String
     
-    init(num: Int, name: String) {
-        self.num = num
-        self.name = name
-    }
-    
     init(num: Int) {
-        self.init(num: num, name: "jeong")
+        self.num = num
+        self.name = "jeong"
     }
 }
-
-let classA = ClassA(num: 10)
-let structA = StructA(num: 10)
-
-print(classA.num)
-print(structA)
