28c9353258efb8c145f8dd3e41d34d38b4b090cd
유정주 <dbwjdwn9216@gmail.com>
2023-08-10 18:06:08 +0900

    JeongImageCache 테스트 프로젝트

---
+++ JICExample/JICExample/PosterCollectionViewCell.swift
@@ -0,0 +1,51 @@
+//
+//  PosterCollectionViewCell.swift
+//  JICExample
+//
+//  Created by 유정주 on 2023/08/10.
+//
+
+import UIKit
+import Kingfisher
+
+class PosterCollectionViewCell: UICollectionViewCell {
+    
+    //MARK: - Views
+    private let posterImageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 5
+        
+        return imageView
+    }()
+    
+    //MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.addSubview(posterImageView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        posterImageView.frame = contentView.bounds
+    }
+    
+    //MARK: - Methods
+    func configure(with urlString: String) {
+        guard let url = URL(string: urlString) else { return }
+        
+        posterImageView.kf.setImage(with: url)
+    }
+    
+    func cancelDownloadImage() {
+        posterImageView.kf.cancelDownloadTask()
+    }
+}
---
+++ JICExample/JICExample/ViewController.swift
@@ -0,0 +1,111 @@
+//
+//  ViewController.swift
+//  JICExample
+//
+//  Created by 유정주 on 2023/08/10.
+//
+
+import UIKit
+
+extension UICollectionReusableView {
+    static var identifier: String {
+        String(describing: self)
+    }
+}
+
+class ViewController: UIViewController {
+    enum Sections: Int {
+        case main
+    }
+
+    private typealias DataSource = UICollectionViewDiffableDataSource<Sections, String>
+    private typealias SnapShot = NSDiffableDataSourceSnapshot<Sections, String>
+    
+    //MARK: - Views
+    lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero,
+                                              collectionViewLayout: createCollectionViewLayout())
+        
+        collectionView.register(PosterCollectionViewCell.self, forCellWithReuseIdentifier: PosterCollectionViewCell.identifier)
+        
+        return collectionView
+    }()
+
+    //MARK: - Properties
+    private let testItem: [String] = Array(repeating: "https://picsum.photos/500", count: 100)
+
+    //MARK: - Life Cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupUI()
+                
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        collectionView.frame = view.safeAreaLayoutGuide.layoutFrame
+    }
+    
+    //MARK: - Setup
+    private func setupUI() {
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(collectionView)
+    }
+    
+    private func createCollectionViewLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection in
+            let itemSize = NSCollectionLayoutSize(widthDimension: .absolute(150),
+                                                  heightDimension: .absolute(220))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            item.contentInsets = .init(top: 0, leading: 5, bottom: 0, trailing: 5)
+            
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: itemSize, subitems: [item])
+            
+            let section = NSCollectionLayoutSection(group: group)
+            section.orthogonalScrollingBehavior = .continuous
+            let sectionInset = NSDirectionalEdgeInsets(top: 0, leading: 10, bottom: 20, trailing: 10)
+            section.contentInsets = sectionInset
+            
+            let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                    heightDimension: .estimated(30))
+            let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(
+                layoutSize: headerSize,
+                elementKind: UICollectionView.elementKindSectionHeader,
+                alignment: .top)
+            section.boundarySupplementaryItems = [sectionHeader]
+            
+            return section
+        }
+        
+        return layout
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        guard let cell = cell as? PosterCollectionViewCell else { return }
+        
+        cell.cancelDownloadImage()
+    }
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return testItem.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PosterCollectionViewCell.identifier, for: indexPath) as? PosterCollectionViewCell else {
+            return .init()
+        }
+        
+        cell.configure(with: testItem[indexPath.row])
+        
+        return cell
+    }
+}
