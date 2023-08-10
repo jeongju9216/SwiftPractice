a24eb0f336cc1a6929bc67d4657cf6e58ebb053c
유정주 <dbwjdwn9216@gmail.com>
2023-08-10 18:15:27 +0900

    JIC 삽입

--- JICExample/JICExample/PosterCollectionViewCell.swift
+++ JICExample/JICExample/PosterCollectionViewCell.swift
@@ -40,9 +40,9 @@ class PosterCollectionViewCell: UICollectionViewCell {
     
     //MARK: - Methods
     func configure(with urlString: String) {
-        guard let url = URL(string: urlString) else { return }
+//        guard let url = URL(string: urlString) else { return }
         
-        posterImageView.kf.setImage(with: url)
+        posterImageView.setImageUsingJIC(url: urlString)
     }
     
     func cancelDownloadImage() {
