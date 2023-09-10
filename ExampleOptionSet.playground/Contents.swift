3d1d7568af7f04b6fe8826de046593a4aa88365b
유정주 <dbwjdwn9216@gmail.com>
2023-09-10 21:17:32 +0900

    OptionSet 연습

---
+++ ExampleOptionSet.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+struct Permissions: OptionSet {
+    static let read = Permissions(rawValue: 1 << 0)
+    static let write = Permissions(rawValue: 1 << 1)
+    static let execute = Permissions(rawValue: 1 << 2)
+
+    let rawValue: Int
+}
+
+
+var userPermissions: Permissions = [.read, .read, .write, .execute]
+print(userPermissions.rawValue.nonzeroBitCount)
+
+if userPermissions.contains(.read) {
+    print("읽기 권한이 있습니다.")
+}
+
+if userPermissions.contains(.write) {
+    print("쓰기 권한이 있습니다.")
+}
+
+if userPermissions.contains(.execute) {
+    print("실행 권한이 있습니다.")
+}
