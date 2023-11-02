685b650c43586fc278a0b010145dec4e3583cd2d
유정주 <dbwjdwn9216@gmail.com>
2023-11-02 12:46:53 +0900

    로컬 라이브러리 연습

---
+++ PackageExample/PackageExample/LocalPackage/Sources/LocalPackage/LocalPackage.swift
@@ -0,0 +1,13 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public struct Jeong {
+    
+    public init() {
+        
+    }
+    
+    public func hi() {
+        print("HI")
+    }
+}
---
+++ PackageExample/PackageExample/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  PackageExample
+//
+//  Created by 유정주 on 11/1/23.
+//
+
+import UIKit
+import LocalPackage
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let jeong = Jeong()
+        jeong.hi()
+    }
+
+    
+}
+
